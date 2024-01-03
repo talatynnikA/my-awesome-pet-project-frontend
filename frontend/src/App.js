ac7bc5f39f13568b2b516c612ccd5bf1598d3a71
Artyom Talatynnik <a.talatynnik@onesoil.ai>
2024-01-03 18:50:08 +0100

    Changed from Switch to Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  //  Routes instead of Switch
 import Home from './pages/Home';
 import Contacts from './pages/Contacts';
 
 function App() {
-  return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/contacts" component={Contacts} />
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>  {/* Changed from Switch to Routes */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
